10f2d74b1e6d6b1a0356660a0f2bd01c97fc35a9
IT21219498 <99394296+IT21219498@users.noreply.github.com>
2022-03-17 06:45:28 +0000

    create classes and use setters and getters

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,18 @@
 #include "Student.h"
 #include <iostream>
+#include <string.h>
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
-  
+void Student::assignDetails(int sID, char sName[]) 
+{
+    studentId = sID;
+    strcpy(name,sName);
 }
 
 // Display StudentId and Name
-Student::display() {
-  
+void Student::display() 
+{
+  cout<<"Student ID is "<<studentId<<endl;
+  cout<<"Student name is "<<name<<endl;
 }
